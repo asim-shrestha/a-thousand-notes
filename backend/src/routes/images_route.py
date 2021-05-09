72802af8b0ba42dd8aeca113b53a23a9028f569a
Asim Shrestha <ashresth@sfu.ca>
2021-05-09 11:22:27 -0700

    feat: add a delete route, refactor into service class

--- backend/src/routes/images_route.py
+++ backend/src/routes/images_route.py
@@ -2,51 +2,24 @@ from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, File, UploadFile
 from typing import List
 
-from .. import utils
 from ..models.database import ApiSession
 from ..models.images import Image 
 from ..schemas.images_schema import ImageBase
 
 from PIL import Image as PILImage
 import io
+from . import image_service
 
 router = APIRouter()
-@router.get("/{image_id}", response_model=ImageBase)
-def get_image(image_id: int, db: Session = Depends(ApiSession)):
-    image = db.query(Image).filter(Image.id == image_id).first()
-    if image:
-        return image
-    else:
-        raise HTTPException(status_code=404, detail="Image not found")
 
+@router.get("/{image_id}", response_model=ImageBase)
+def get_image_by_id(image_id: int, db: Session = Depends(ApiSession)):
+    return image_service.get_image_by_id(image_id, db)
 
 @router.post("/", response_model=List[ImageBase])
 def create_images(files: List[bytes] = File(...), db: Session = Depends(ApiSession)):
-    # Test if the file is an image by attempting to read it with PIL
-    for file in files:
-        try:
-            PILImage.open(io.BytesIO(file))
-        except:
-            raise HTTPException(status_code=400, detail="You have uploaded one or more invalid images. Aborting")
+    return image_service.create_images(files, db)
 
-    # Upload to firebase and create entities
-    image_urls = [utils.upload_to_firebase(file) for file in files]
-    # Get random tracks from spotify for each file
-    random_tracks = [utils.get_random_track() for file in files]
-
-    # Create records with data
-    db_images = []
-    for i in range(len(files)):
-        db_images.append(Image())
-        db_images = [Image(
-            image_url=image_urls[i],
-            spotify_name=random_tracks[0].name,
-            spotify_uri=random_tracks[0].uri,
-            spotify_preview_url=random_tracks[0].preview_url,
-            spotify_popularity=random_tracks[0].popularity,
-        )]
-
-    # Save to db
-    db.add_all(db_images)
-    db.commit()
-    return db_images
+@router.delete("/{image_id}", response_model=None)
+def delete_images_by_ids(image_id: int, db: Session = Depends(ApiSession)):
+    return image_service.delete_images_by_ids(image_id, db)
--- backend/tests/test_routes/test_images/test_images.py
+++ backend/tests/test_routes/test_images/test_images.py
@@ -43,3 +43,25 @@ def test_multi_upload_fails_with_one_non_image(text_file, image_file):
         ("files", text_file),
     ])
     assert response.status_code == 400
+
+def test_cannot_delete_non_existant_image():
+    non_existant_id = 967
+    response = client.delete(f"{IMAGE_ROUTE}/{non_existant_id}")
+    assert response.status_code == 404
+
+def test_create_get_delete(image_file):
+    response = client.post(f"{IMAGE_ROUTE}/", files={
+        "files": image_file
+    })
+
+    assert response.status_code == 200
+    id = response.json()[0]['id']
+
+    response = client.get(f"{IMAGE_ROUTE}/{id}")
+    assert response.status_code == 200
+
+    response = client.delete(f"{IMAGE_ROUTE}/{id}")
+    assert response.status_code == 200
+
+    response = client.get(f"{IMAGE_ROUTE}/{id}")
+    assert response.status_code == 404
