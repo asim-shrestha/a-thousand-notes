40dbae7cb26ed2e458ef3a6e5dfff909b95764a2
Asim Shrestha <ashresth@sfu.ca>
2021-05-09 16:10:45 -0700

    feat: link spotify uri

--- frontend/components/ImageCard.tsx
+++ frontend/components/ImageCard.tsx
@@ -5,20 +5,29 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
+import LinkIcon from '@material-ui/icons/Link';
+import { Button } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import ImageInfo from '../types/ImageInfo';
+import ReactAudioPlayer from 'react-audio-player';
 
 const useStyles = makeStyles({
   root: {
     margin: '1.5em',
     maxWidth: 345,
   },
+  linkButton: {
+    borderRadius: "2em"
+  }
 });
 
 type ImageCardProps = {imageInfo: ImageInfo}
 const ImageCard = ({ imageInfo }: ImageCardProps) => {
   const classes = useStyles();
+
+  const openInSpotify = () => {
+    location.href = imageInfo.spotify_uri;
+  }
 
   return (
     <Card className={classes.root}>
@@ -34,18 +43,25 @@ const ImageCard = ({ imageInfo }: ImageCardProps) => {
           <Typography gutterBottom variant="h5" component="h2">
             {imageInfo.image_name}
           </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography variant="body2" color="textSecondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
             across all continents except Antarctica
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          Share
-        </Button>
-        <Button size="small" color="primary">
-          Learn More
+        <ReactAudioPlayer
+          src={imageInfo.spotify_preview_url}
+          controls
+        />
+        <Button
+          className={classes.linkButton}
+          variant="contained"
+          color="primary"
+          disableElevation
+          onClick={openInSpotify}
+        >
+          <LinkIcon/>
         </Button>
       </CardActions>
     </Card>
